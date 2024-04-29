ff78cc96969cab73393748ad6f3427b93a6110db
Alonso Jiménez <ajimmenezz@gmail.com>
2024-04-29 11:36:09 -0600

    Add Payment Controller

---
+++ app/Http/Controllers/Authorization/AuthorizationPayment.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers\Authorization;
+
+use Illuminate\Http\Request;
+use App\Exceptions\AuthorizationException;
+
+class AuthorizationPayment extends AuthorizationController
+{
+    public function payment(Request $request)
+    {
+        $authorization = $this->validateRepeatedRequest($request, "PA");
+
+        try {
+            $this->validateHeaders($request->headers->all());
+            $this->validateBodyReversal($request->all());
+            $card = $this->validateCard($request->all()['card_id'], $request->all()['card_number']);
+            $balance = $this->encrypter->decrypt($card->Balance);
+            $newBalance = $balance + $request->all()['values']['billing_value'];
+
+            $card->Balance = $this->encrypter->encrypt(number_format($newBalance, 2, '.', ''));
+            $card->save();
+
+            $this->registerMovement($card->Id, $request->all()['values']['billing_value'], $newBalance, 'PAYMENT');
+
+            $response = $this->dock_response('APPROVED', 'Transaction approved', $newBalance);
+
+            $this->save_response($authorization, $request, $response);
+
+            return response()->json($response, 200);
+        } catch (AuthorizationException $e) {
+            $error = $this->save_error($authorization, $e->getMessage());
+            return response()->json($error, 200);
+        }
+    }
+
+    private function validateBodyReversal($body)
+    {
+        if (!isset($body['card_id'])) {
+            if (!isset($body['card_number'])) {
+                throw new AuthorizationException('Card not found', 200, 400);
+            }
+        }
+
+        if (!isset($body['card_expiration_date'])) {
+            throw new AuthorizationException('Card expiration date is required', 200, 400);
+        }
+
+        if (!isset($body['values']['billing_value'])) {
+            throw new AuthorizationException('Transaction amount must be greater than zero', 200, 400);
+        }
+    }
+}
