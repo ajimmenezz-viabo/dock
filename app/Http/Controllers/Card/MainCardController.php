237759563f21fcb07fc5702eb001daf75a1f238b
Alonso Jiménez <ajimmenezz@gmail.com>
2024-06-17 18:08:14 -0600

    Modify Batch proccess

--- app/Http/Controllers/Card/MainCardController.php
+++ app/Http/Controllers/Card/MainCardController.php
@@ -204,7 +204,7 @@ class MainCardController extends Controller
             $prefix = null;
             $client_id = null;
             foreach ($external_card->metadata as $metadata) {
-                if ($metadata->key == 'text1') {
+                if (isset($metadata->key) && $metadata->key == 'text1') {
                     $prefix = substr($metadata->value, 0, 2);
                     $client_id = intval(substr($metadata->value, 2));
                     break;
